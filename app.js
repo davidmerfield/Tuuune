ab1a738e4ec134e8aa5393be926f82f01400401e
David Merfield <dmerfield@gmail.com>
2014-04-20 10:48:15 -0700

    isodatestring
    for making youtube api format

--- app.js
+++ app.js
@@ -192,6 +192,15 @@ function loadUtilities() {
       id: [],
       key: apiKey
     },   
+    ISODateString: function (d){
+     function pad(n){return n<10 ? '0'+n : n}
+     return d.getUTCFullYear()+'-'
+          + pad(d.getUTCMonth()+1)+'-'
+          + pad(d.getUTCDate())+'T'
+          + pad(d.getUTCHours())+':'
+          + pad(d.getUTCMinutes())+':'
+          + pad(d.getUTCSeconds())+'Z'
+    },
     makeQueryURL: function (query, defaults) {
       
       var url = '';
