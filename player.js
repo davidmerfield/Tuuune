5da24037c2309b6b7ad7f1f199bdb1e4a97603e2
David Merfield <dmerfield@gmail.com>
2014-04-21 18:01:36 -0700

    bound player to ui in index

--- player.js
+++ player.js
@@ -1,49 +1,107 @@
+var util = loadUtilities();
+
+$(function() {
+
+    var params = { allowScriptAccess: "always" };
+
+    swfobject.embedSWF(    
+        "http://www.youtube.com/v/HWWogO3CWXs&enablejsapi=1&playerapiid=embed", "embed", "425", "365", "8", null, null, params);
+
+
+function play() {      
+  ytplayer.playVideo();
+}
+
+function pause() {      
+  ytplayer.pauseVideo();
+}
+function stop() {      
+  ytplayer.stopVideo();
+}
+
+function newVideo() {
+  ytplayer.loadVideoById('FgxwKEuy-pM', 0)
+  cueVideoById
+}
+
+});
+
+
+function onYouTubePlayerReady (playerID) {
+  console.log('Player ready');
+  embed.addEventListener('onStateChange', 'player.stateChange');
+};
+
 var player = {
-   videoLength: 0,
+  videoLength: 0,
    init: function(){
       this.renderUI();
+  },
+  video: '',
+  stateChange: function(e){
+    console.log(e + ' is player state.');
+
+    // Playing
+    if (e === 1) {
+      $('#pause').show();
+      $('#play').hide();
+      setInterval(player.updateProgressBar, 50);
+    }
+
+    // Paused
+    if (e === 2) {
+      $('#pause').hide();
+      $('#play').show();
+      clearInterval(player.updateProgressBar)
+    }
+
    },
-   videoID: '',
-   play: function(){
-      if (embed.getPlayerState() === '2') {
-         embed.playVideo()
+   play: function(video){
+
+      if (video) {
+        this.video = video;
+      } 
+
+      if (embed.getPlayerState() === 2) {
+         return embed.playVideo()
       };
-      if (this.videoID === '') {
-         this.videoID = Window.queue[0].id
+      
+      if (this.video === '') {
+         this.video = Window.queue[0]
       };
-      console.log(this.videoID)
-      embed.loadVideoById(this.videoID);
-      embed.playVideo();
+
       this.renderUI();
-   },
-   setVideo: function(id){
-      this.videoID = id;
+      embed.loadVideoById(this.video.id);
    },
    updateProgressBar: function(){
       
+      var duration = player.duration;
+
       var currentTime = embed.getCurrentTime(),
-          progress = currentTime/this.videoLength*100,
-         buffered = embed.getVideoLoadedFraction()/this.videoLength*100;
+          progress = currentTime/duration*100,
+         buffered = embed.getVideoLoadedFraction()*100;
 
       $('#progress').width(progress + '%');
       $('#buffered').width(buffered + '%');
 
       var mins = Math.floor(currentTime / 60),
-         seconds = pad(Math.floor(currentTime % 60),2);
+         seconds = util.pad(Math.floor(currentTime % 60),2);
 
       $('#time').text(mins + ':' + seconds);
-
-      this.videoLength = embed.getDuration();
-      
-      var totalMins = Math.floor(this.videoLength /60),
-          totalSeconds = pad(Math.floor(this.videoLength % 60),2);
-
-      $('#duration').text(totalMins + ':' + totalSeconds);
-
    },
    renderUI: function () {
 
-      setInterval(this.updateProgressBar, 50);
+    player.duration = embed.getDuration();
+
+    var totalMins = Math.floor(player.duration/60),
+        totalSeconds = util.pad(Math.floor(player.duration % 60),2);
+
+    $('#time').text('0:00');
+
+    $('#duration').text(totalMins + ':' + totalSeconds);
+
+    $('#title').text(player.video.snippet.title);
+
 
 
    }
@@ -58,13 +116,28 @@ $('#pause').click(function(){
    embed.pauseVideo();
 });
 
+$('#previous').click(function(){
+  Window.queue.unshift(Window.playHistory.shift());
+  var video = Window.queue[0];
+  player.play(video)
+});
 
+$('#next').click(function(){
+  Window.playHistory.push(Window.queue[Window.currentSong]);
+  Window.currentSong++;
+  var video = Window.queue[Window.currentSong];
+  player.play(video)
+});
 
-function pad(num, size) {
-          var s = num+"";
-          while (s.length < size) s = "0" + s;
-          return s;
-      }
+$('#bar').click(function(e){
+  var xOffset = e.pageX - $(this).offset().left,
+      ratio = xOffset/$(this).width(),
+      seconds = Math.round(ratio*player.duration);
+
+  embed.seekTo(seconds, true)
+
+});
+
  
 
 
