613de92a494b7210a0234911aeeecfcbd4049f88
David Merfield <dmerfield@gmail.com>
2014-04-21 17:59:09 -0700

    util tidy title
    for removing unwanted items from a title

--- util.js
+++ util.js
@@ -87,3 +87,82 @@ function loadUtilities() {
       }
       return false
     },
+    tidyTitle: function (title) {
+        
+        console.log('initial title: ' + title);
+
+        title = title.replace(/ *\[[^)]*\] */g, "");
+
+        var getParens = /\((.*?)\)/;
+            
+
+            title = removeBanned(title, [
+              'official video',
+              'official audio',
+              'official music video',
+              'audio stream',
+              'new release',
+              'studio version',
+              'lyrics'
+            ]);
+
+            function removeBanned(string, phrases) {
+              for (var i in phrases){
+                var phrase = phrases[i];
+                    regex = new RegExp( '(' + ' ' + phrase + ')', 'gi' );
+                
+                string = string.replace(regex, " " );
+              }
+              return string
+            }
+
+            if (title.match(getParens)) {
+
+              parensContent = title.match(getParens)[0];
+
+              console.log(parensContent);
+
+              if (parensContent.indexOf('remix') === -1 && parensContent.indexOf('Remix') === -1) {
+                title = title.replace(/ *\[[^)]*\] */g, "").replace(/ *\([^)]*\) */g, "");
+              }
+
+            }
+
+            console.log('final title: ' + title);
+            
+            title = title.trim();
+            
+            title = title.replace(/"/g, '');
+
+            while (title.substr(title.length - 1) === '-') {
+              title = title.slice(0, - 1);
+            }
+
+            return title
+    },
+    ISODateString: function (d){
+     function pad(n){return n<10 ? '0'+n : n}
+     return d.getUTCFullYear()+'-'
+          + pad(d.getUTCMonth()+1)+'-'
+          + pad(d.getUTCDate())+'T'
+          + pad(d.getUTCHours())+':'
+          + pad(d.getUTCMinutes())+':'
+          + pad(d.getUTCSeconds())+'Z'
+    },
+    makeQueryURL: function (query, defaults) {
+      
+      var url = '';
+          baseURL = 'https://www.googleapis.com/youtube/v3/';
+
+      for (key in defaults) {
+        var param = defaults[key];
+        if (typeof param == 'object'){
+            param = param.join(',');
+        };
+        url += encodeURIComponent(key) + '=' + encodeURIComponent(param) + '\&'
+      };
+
+      return baseURL + query + '?' + url
+    }
+  }
+}
