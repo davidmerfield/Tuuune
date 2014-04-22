e4e40eeabafeaeed79106896471535bcd7fa040d
David Merfield <dmerfield@gmail.com>
2014-04-21 17:58:24 -0700

    util pad
    for making '3:4' into '03:04'

--- util.js
+++ util.js
@@ -74,3 +74,8 @@ function loadUtilities() {
       }
       return duration
     },
+    pad: function(num, size) {
+              var s = num+"";
+              while (s.length < size) s = "0" + s;
+              return s;
+    },
