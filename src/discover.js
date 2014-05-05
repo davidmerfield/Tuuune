6fbbb21f78394a2339035c70b0cd8372609a1d70
David Merfield <dmerfield@gmail.com>
2014-05-04 21:51:28 -0700

    removed filter

--- src/discover.js
+++ src/discover.js
@@ -62,263 +62,31 @@ var discover =  function () {
     function searchComplete () {
 
       // Determine if we need to keep searching
-      if (filteredSongs.length < minResults) {
+      if (filteredSongs.length < options.minResults) {
         return searchForSongs(callback)
       } 
 
-      return callback('Search complete') 
+      return callback('Found ' + filteredSongs.length + ' songs!') 
 
     };    
 
-  }
-
-  function filter (songs) {
-
-    console.log('FILTER: TOTAL SONGS: ' + songs.length);
-
-     var options = getOptions('filter'),
-         results = [];
-
-     options.maxDuration = 7200;
-     options.minDuration = 60;
-
-     var bannedWords = [
-      'live', 'radio', 'choir', 'medley', 'monologue',
-      'university', 'college', 'encore', 'duet',
-      'moshcam', 'premiere', 'dvd', 'Eurovision', 'sxsw',
-      'improv', 'TEDx', 'band', 'hd', 'quartet', 'choral', 'chorale',
-      'concerto', 'requiem', 'improvisation', 'semifinal', 'recital',
-      'festival', 'suite', 'idol', 'contest', 'Q&A', 'middle school',
-      'high school', 'capella', 'a cappella', 'bonaroo', 'subscribers',
-      'octet', 'best of', 'barbershop', 'recording', 'song',
-      'music', 'orchestra', 'backstage', 'behind the scenes', 'parody', 'making of',
-      'w/', 'rehearsal', 'acoustic', 'tour', 'part', '@', '#',
-      'violin', 'piano', 'hall of fame', 'documentary',
-      'awards', 'music theory', 'arrangement',
-      'lesson', 'tabs', 'tutorial', 'theme', 'kickstarter', 'session',
-      'blog', 'vlog', 'concert', 'concierto', 'interview', 'soundtrack',
-      'chord changes', 'instrumental', 'episode', 'performance',
-      'letterman', 'jimmy kimmel', 'jonathan ross', 'me singing',
-      'me covering', 'guitar tab', 'performed by',
-      'dubstep', 'jazz', 'rap', 'hiphop', 'anthem',
-      'version', 'review', '.wmv',
-      '.mov', '.avi', '.mp3', '.mpg', 'at the', 'OST'
-      ];
-
-     bannedWords.push(
-      'Justin Bieber', 'One Direction', 'Michael Jackson', 'Temper Trap',
-       'dean martin', 'justin timberlake', 'cee lo green', 'josh groban',
-       'britney spears', 'modest mouse', 'elton john', 'bob dylan', 'don mclean',
-       'katy perry', 'johnny cash', 'snow patrol', 'nickleback', 'idina menzel',
-       'timbaland', 'ingrid michaelson', 'shakira', 'lana del rey', 'alabama shakes',
-       'radiohead', 'paloma faith', 'the ramones', 'snoop dogg', 'bach', 'the shins',
-       'fleet foxes', 'led zeppelin', 'taio cruz', 'birdy', 'paolo nutini', 'willie nelson',
-       'iron maiden', 'Guns N\' Roses', 'guns n roses', 'tom waits', 'REM', 'R.E.M',
-       'R. Kelly', 'R.Kelly', 'bon iver', 'sinead o connor', 'norah jones', 'hq',
-       'red hot chilli peppers', 'paul mcartney', 'taylor swift', 'jason derulo',
-       'billy bragg', 'miley cyrus', 'p!nk', 'modest mouse', 'wu tang clan', 'ACDC', 'AC/DC',
-       'macklemore', 'queens of the stone age', 'the kinks', 'jimi hendrix',
-       'twisted sister', '2pac', 'bjork', 'enya', 'jethro tull', 'mariah carey',
-       'lupe fiasco', 'my chemical romance', 'the beatles', 'maroon 5', 'carly rae jepsen',
-       'the wanted', 'david bowie', 'rod stewart', 'rolling stones', 'skrillex', 'cher lloyd'
-     );
-
-     if (options.category !== 'album') {
-        bannedWords.push('album', 'full album', 'remixxx', 'remixed', 'remake');
-        options.maxDuration = 720; // only songs up to 12 min in duration
-     }
-
-     if (options.category !== 'song') {
-        options.minDuration = 1442 // only albums more than 24 min in duration
-     }
-
-     if (options.exclude.covers) {
-        bannedWords.push('cover', 'covered', 'covers', 'tribute')
-     }
-
-     if (options.exclude.remixes) {
-        bannedWords.push('remix', 'remixx', 'remixxx', 'remixed')
-     }
-
-     if (options.exclude.foreign) {
-        var nonEnglish = new RegExp("[^\x00-\x7F]+");
-     }
-
-     for (var i in songs) {
-       
-       var song = songs[i],
-           listens = song.statistics.viewCount,
-           dislikes = song.statistics.dislikeCount,
-           likes = song.statistics.likeCount;
-
-        song.duration = helper.parseYTDuration(song.contentDetails.duration);
-
-       // Check if video has too few or too many views 
-       if (listens > options.maxListens ||
-           listens < options.minListens) {
-         continue
-       }
-       // Check if video is well liked
-       if (dislikes / likes > options.dislikesToLikes) {
-         continue
-       } 
-
-       // ignore non english songs
-       if(nonEnglish && nonEnglish.test(song.snippet.title)) {
-         continue
-       }
-
-       // disallow duplicates
-       for (var j in results) {
-         if (results[j].id === song.id) {
-           continue
-         }
-       }
-
-       // Check video has enough likes
-       if (likes / listens < options.likestoListens){
-         continue
-       }
-
-       // skip banned words
-       if (helper.hasBanned(song.snippet.title, bannedWords)) {
-         continue
-       }
-
-       // probably not a song
-       if (song.snippet.title.indexOf('#') > -1 ||
-           song.snippet.title.indexOf('@') > -1 ) {
-         continue
-       }
-
-       // probably not a song
-       if (song.snippet.title.indexOf(' - ') === -1) {
-         continue
-       }
-
-       // Ignore too short or too long videos
-       if (song.duration < options.minDuration ||
-           song.duration > options.maxDuration)  {
-         continue
-       };
-       
-       // Tidy up title string      
-       song.prettyTitle = helper.tidyTitle(song.snippet.title);
-
-
-       // Song title contains a number
-       if (/\d/.test(song.prettyTitle)) {
-        continue
-       }
-
-       // Probably not great
-       if (song.prettyTitle.length > 75) {
-         continue
-       }
-
-       // Make pretty duration
-       var mins = Math.floor(song.duration / 60),
-           seconds = helper.pad(Math.floor(song.duration % 60),2);
-        
-       song.prettyDuration = mins + ':' + seconds;
-
-       // Make pretty viewcount
-       song.prettyViewCount = Math.round(listens/1000) + 'k';
-       
-       // passed tests, add to queue
-       results.push(song);        
-
-     };
-
-     console.log('PASSED SONGS: ' + results.length);
-
-     return results
   }; 
 
-  function setOptions (defaultValues) {
-     
-     var searchDefaults = {
-        // topicID: '/m/074ft', // all songs
-        regionCode: 'US'
-     },
-
-     filterDefaults = {
-        likestoListens: 0.005, // ratio of likes to views
-        dislikesToLikes: 0.05, // ratio of likes to dislikes
-        minListens: 1000,
-        maxListens: 100000,
-        category: 'song',
-        exclude: {
-           foreign: true,
-           covers: true,
-           remixes: true,
+  function removeSongByID (id) {
+    for (var i in allSongs) {
+        var song = allSongs[i];
+        if (song.id && song.id === id) {
+          allSongs = allSongs.splice(i,1);
+          break 
         }
-     };
-
-     // If we want defaults or if no options have been set yet
-     if (defaultValues) {
-        
-        // Clone default properties, a reference would cause issues
-        options.search = helper.duplicate(searchDefaults);
-        options.filter = helper.duplicate(filterDefaults);
-     };
-     
-  };
-
-  function getOptions (category, defaultValues) {
-
-     if (category === 'search') {
-        return options.search
-     };
-
-     if (category === 'filter') {
-        return options.filter
-     };
-     
-     // All the options!
-     return {
-        search: options.search,
-        filter: options.filter
-     };
-
-  };   
-
-  function addOptionsListener () {
-
-     $('.option').on('change', function(){
-
-        console.log('options changed!!!!');
-
-        var name = $(this).attr('id'),
-            category = $(this).attr('category');
-        
-        if (name === 'maxListens') {
-           options.filter.maxListens = parseInt($(this).val())
-        } else {
-           options[category][name] = $(this).val();
-        }
-
-        if (category == 'search') {
-          filteredSongs = [];
-          allSongs = [];
-
-          render('empty');
-          
-          return findSongs()
-        }
-        // refilter and rerender with new options
-        filteredSongs = filter(allSongs);
-        render('done')
-
-        if (filteredSongs.length < 10) {
-           searchForSongs()
-        }
-
-     });
-  };
-
-  function removeOptionsListener () {
-     $('.option').unbind('on');
+    };      
+    for (var i in filteredSongs) {
+       var song = filteredSongs[i];
+       if (song.id && song.id === id) {
+        filteredSongs = filteredSongs.splice(i,1);
+         break 
+       }
+    }
   };
 
   function setOptionsVals () {
