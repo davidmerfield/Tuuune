809b3d4b492e2385eab46d422de76af76d73b102
David Merfield <dmerfield@gmail.com>
2014-05-05 19:49:55 -0700

    started starred view

---
+++ src/app/controllers/starred.js
@@ -0,0 +1,21 @@
+function starred () {
+   
+   var starredSongs = [];
+
+   function star () {
+      
+   }
+
+   function unStar () {
+
+   }
+
+   function render () {
+
+   }
+
+   return function init () {
+
+   }
+   
+}
