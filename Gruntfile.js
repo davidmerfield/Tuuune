e2193e384b53444c863d2780bad51f2ca165ad75
David Merfield <dmerfield@gmail.com>
2014-05-05 19:44:27 -0700

    added grunt for merging multiple js files

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+   // Project configuration.
+   grunt.initConfig({
+      watch: {
+         js: {
+            files:  [ 'src/*.js' ],
+            tasks:  [ 'uglify' ]
+         }
+      },
+      uglify: {
+         my_target: {
+            options: {
+               mangle: false,
+               beautify: true
+            },
+            files: {
+               'public/js/libraries.js': [
+                  'src/libraries/*.js',
+               ],
+               'public/js/app.js': [
+                  'src/util/*.js',
+                  'src/app/models/*.js',
+                  'src/app/controllers/*.js',
+                  'src/app/views/*.js',
+                  'src/init.js'
+               ]
+            }
+         }
+      }
+   });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+   //  watch
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'watch']);
+
+};
