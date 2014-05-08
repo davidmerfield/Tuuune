f3fd171f040362dd9d8442ac0691d319c7730c69
David Merfield <dmerfield@gmail.com>
2014-05-07 19:41:27 -0700

    fleshed out youtube player methods

--- src/app/controllers/youtubePlayer.js
+++ src/app/controllers/youtubePlayer.js
@@ -1,48 +1,65 @@
 var youtubePlayer = function () {
 
-  var player;
-
   function init (callback) {
 
     var params = {allowScriptAccess: "always"};
 
-    swfobject.embedSWF("http://www.youtube.com/v/pYVW0I-NnDc&enablejsapi=1&playerapiid=youtubeEmbed", "youtubeEmbed", "425", "365", "8", null, null, params);
+    swfobject.embedSWF(
+      "http://www.youtube.com/v/pYVW0I-NnDc&enablejsapi=1&playerapiid=youtubeEmbed",
+      "youtubeEmbed", "425", "365", "8", null, null, params
+    );
 
     window.onYouTubePlayerReady = function (playerID) {
-      
+
       console.log('Youtube player loaded.');
-
       youtubeEmbed.addEventListener('onStateChange', 'youtubePlayer.stateChange');
-
       callback();
-
     };
-
   }
-  
+
   function play (song) {
-      if (song) {
-         youtubeEmbed.loadVideoById(song.sourceID)
-      }
-      else {
-        youtubeEmbed.playVideo()
-      }
-   }
+    if (song) {
+      youtubeEmbed.loadVideoById(song.sourceID)
+    }
+    else {
+      youtubeEmbed.playVideo()
+    }
+  }
 
-   function pause () {
-      player.pauseVideo()
-   }
+  function pause () {
+    youtubeEmbed.pauseVideo()
+  }
 
-   function currentState () {
-      
-   }
+  function stop () {
+    youtubeEmbed.stopVideo()
+  }
 
-   return {
-      init: init,
-      play: play,
-      pause: pause,
-      currentState: currentState
-   } 
+  function seekTo(seconds) {
+    youtubeEmbed.seekTo(seconds, true);
+  }
+
+  function getProgress() {
+
+    var currentTime = youtubeEmbed.getCurrentTime(),
+        mins = Math.floor(currentTime / 60),
+        seconds = helper.pad(Math.floor(currentTime % 60),2);
+
+    return {
+      currentTime: currentTime,
+      duration: youtubeEmbed.getDuration(),
+      playedPercent: currentTime/youtubeEmbed.getDuration()*100,
+      bufferedPercent: youtubeEmbed.getVideoLoadedFraction()*100
+    }
+  }
+
+  return {
+    init: init,
+    play: play,
+    pause: pause,
+    getProgress: getProgress,
+    seekTo: seekTo
+  }
+
 };
 
 youtubePlayer.stateChange = function(e) {
