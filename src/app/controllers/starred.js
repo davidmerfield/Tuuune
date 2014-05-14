50e49f6438c080b2499347333f0db064fd98a182
David Merfield <dmerfield@gmail.com>
2014-05-14 13:38:04 -0700

    added starred songs view

--- src/app/controllers/starred.js
+++ src/app/controllers/starred.js
@@ -1,21 +1,92 @@
-function starred () {
+var starred = (function(){
    
-   var starredSongs = [];
+   var storageKey = 'musicFinder:starred';
 
-   function star () {
+   var exports = {
+         init: init,
+         hide: hide,
+         star: star,
+         unstar: unstar,
+         reset: reset,
+         getSongs: getSongs
+       };
+   
+   function init () {
+
+      $('#starred').show();
+
+      var songs = getSongs();
+
+      render(songs);  
+
+   };
+
+   function hide () {
+      $('#starred').hide();
+   };
+
+   function reset() {
+      localStorage.removeItem(storageKey);
+   };
+
+   function getSongs () {
       
-   }
+      var songs = localStorage.getItem(storageKey);
 
-   function unStar () {
+      if (songs) {
+         return JSON.parse(songs);
+      } else {
+         setSongs({});
+         return {}
+      };
 
-   }
+   };
 
-   function render () {
+   function setSongs (songs) {
+      return localStorage.setItem(storageKey, JSON.stringify(songs));
+   };
 
-   }
+   function star (song) {
+      
+      var songs = getSongs();
+          songs[song.id] = song;
 
-   return function init () {
+         setSongs(songs);
 
-   }
-   
-}
+   };
+
+   function unstar (song) {
+
+      var songs = getSongs();
+
+          delete songs[song.id];
+
+          setSongs(songs);
+
+
+   };
+
+   function render(songs) {
+
+      if (songs) {
+
+         console.log(songs);
+
+         var html = '';
+
+         for (var i in songs) {
+           var song = songs[i];
+           html += Song().render(song);
+         }
+
+         console.log(html);
+
+         $('#starred .songList').html(html);
+
+      }
+
+   };
+
+   return exports
+
+}());
