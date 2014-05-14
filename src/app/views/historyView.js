a4a1d2cf16f915375f9dde3909bd57db92d05098
David Merfield <dmerfield@gmail.com>
2014-05-14 13:35:48 -0700

    added new files for each view

---
+++ src/app/views/historyView.js
@@ -0,0 +1,9 @@
+var historyView = (function(){
+
+   var template = 
+      '<h1>Play history</h1>' +
+      '<section class="songList"></section>';
+   
+   return template    
+}());
+
---
+++ src/app/views/queueView.js
@@ -0,0 +1,9 @@
+var queueView = (function(){
+
+   var template = 
+      '<h1>Play queue</h1>' +
+      '<section class="songList"></section>';
+   
+   return template    
+}());
+
