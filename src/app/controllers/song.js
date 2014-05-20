b24d5e789e417f6bfabf71b72f6b77864827d221
David Merfield <dmerfield@gmail.com>
2014-05-19 18:10:44 -0700

    added song list methods to Song

--- src/app/controllers/song.js
+++ src/app/controllers/song.js
@@ -2,7 +2,12 @@ var Song = (function(){
 
    var exports = {
       init: init,
-      render: render
+      render: render,
+      get: get,
+      getSongsAfter: getSongsAfter,
+      add: add,
+      drop: drop
+
    };
 
    var template = 
@@ -48,6 +53,57 @@ var Song = (function(){
 
    function render(song) {
       return Mustache.render(template, song);
+   };
+
+   function get (id, songlist) {
+
+     for (var i in songlist) {
+       
+       if (songlist[i].id && songlist[i].id === id) {
+         return songlist[i]
+       };
+     };
+   };
+
+   function getSongsAfter(song, songlist) {
+
+     for (var i in songlist) {
+       
+       if (songlist[i].id && songlist[i].id === song.id) {
+         return songlist.slice(i + 1)
+       };
+     };
+   };
+
+   function drop (id, songlist) {
+     
+     for (var i in songlist) {
+
+       if (songlist[i].id && songlist[i].id === id) {
+         songlist.splice(i, 1);
+       };
+       
+     };
+   };  
+
+   function add (newSongs, songlist) {
+     
+     if (newSongs.length === 0) {return songlist};
+
+     var newSong = newSongs.pop();
+
+     for (var i in songlist) {
+       if (newSong.id === songlist[i].id) {
+         
+         // ignore this item we have it already
+         return add(newSongs, songlist);
+       };
+     };
+
+     // item is unique, add it to old items
+     songlist.push(newSong);
+
+     return add(newSongs, songlist);
    };
 
    return exports;
