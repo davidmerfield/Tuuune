2a70e459ba47e83e2e31d136121ff63a0841ee27
David Merfield <dmerfield@gmail.com>
2014-05-21 19:49:53 -0700

    added event handlers to song controller

--- src/app/controllers/song.js
+++ src/app/controllers/song.js
@@ -2,113 +2,70 @@ var Song = (function(){
 
    var exports = {
       init: init,
-      render: render,
-      get: get,
-      getSongsAfter: getSongsAfter,
-      add: add,
-      drop: drop
-
+      addListener: addListener,
+      removeListener: removeListener
    };
-
-   var template = 
-        '<span class="thumbnail" style="background: url({{thumbnail}}) no-repeat center center;background-size: cover"><img src="" /></span>' +
-         '<span class="title">{{pretty.title}} </span> ' +
-         '<span class="buttons">' +
-           '<span class="playSong">Play</span>' +
-           '<span class="removeSong">Hide</span>' +
-           '<span class="starSong {{#isStarred}} starred {{/isStarred}}">&#9733;</span>' +
-           '<span class="queueSong">+ Queue</span>' +
-         '</span>' + 
-         '<span class="stats">' +
-           '<span class="duration">{{pretty.duration}} &#8226; </span>' +
-           '<span class="views">{{pretty.listens}} listens &#8226; </span>' +
-           '<span class="source">{{source.name}}</span>' +
-         '</span>';
 
    function init () {
 
-      $('body').on('click', '.song .buttons span', function(e){
-         
-         var className = $(this).attr('class'),
-             id = $(this).parent().parent().attr('id');
 
-        if (className === 'starSong') {
-          $(this).toggleClass('starred');
-        };
+   };
 
-        if (className === 'removeSong') {
-          $(this).parent().parent().remove();
-        };
+   var eventHandlers = {
+     playSong: function(song, songList) {
         
-        e.preventDefault(); // stops click event bubbling to .result
- 
-        $(exports).trigger(className, [{id: id}]);
+        player.play(song, songList);
 
-         return false; // stops window scrolling to top
- 
-      });
+     },
+     queueSong: function(song, songList) {
+        
+        player.addToQueue(song);
+
+     },
+     removeSong: function(song, songList, el) {
+
+      songList.remove(song.id);
+      el.parent().parent().remove();
+
+     },
+     starSong: function(song, songList, el) {
+
+      var isStarred = el.attr('data-isStarred');
+
+      if (isStarred === 'starred') {
+        
+        song.isStarred = false;
+        starred.unstar(song);
+        el.attr('data-isStarred', '');
+
+      } else {
+
+        song.isStarred = true;
+        starred.star(song);
+        el.attr('data-isStarred', 'starred')
+      };
+
+     }
+   };
+
+   function addListener (view, songList) {
+
+    $('#' + view).on('click', '.song .buttons span', function(e){
+      
+      var id = $(this).parent().parent().attr('id'),
+          className = $(this).attr('class'),
+          song = songList.find(id);
+
+      eventHandlers[className](song, songList, $(this));
+
+      $('#' + view).trigger(className, {id: id});
+
+    });
 
    };
 
-   function render(song) {
-
-      var songNode = document.createElement('a');
-          songNode.className = 'song';
-          songNode.id = song.id;
-          songNode.innerHTML = Mustache.render(template, song);
-
-      return songNode;
-   };
-
-   function get (id, songlist) {
-
-     for (var i in songlist) {
-       
-       if (songlist[i].id && songlist[i].id === id) {
-         return songlist[i]
-       };
-     };
-   };
-
-   function getSongsAfter(song, songlist) {
-
-     for (var i in songlist) {
-       
-       if (songlist[i].id && songlist[i].id === song.id) {
-         return songlist.slice(i + 1)
-       };
-     };
-   };
-
-   function drop (id, songlist) {
-     
-     for (var i in songlist) {
-
-       if (songlist[i].id && songlist[i].id === id) {
-         songlist.splice(i, 1);
-       };
-       
-     };
-   };  
-
-   function add (newSongs, songlist) {
-     
-     if (newSongs.length === 0) {return songlist};
-
-     var newSong = newSongs.pop();
-
-     for (var i in songlist) {
-       if (newSong.id === songlist[i].id) {
-         
-         // ignore this item we have it already
-         return add(newSongs, songlist);
-       };
-     };
-
-     // item is unique, add it to old items
-     songlist.push(newSong);
-
-     return add(newSongs, songlist);
+   function removeListener (view) {
+    $('#' + view).off();
    };
 
    return exports;
