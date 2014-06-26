0bda8d1087fcc2cf04b2c9bd7ba2d50d85922fb2
David Merfield <dmerfield@gmail.com>
2014-06-25 17:43:14 -0700

    added words to filter blacklist

--- src/app/controllers/filter.js
+++ src/app/controllers/filter.js
@@ -4,7 +4,7 @@ var filter = function (songs, options) {
        banned = { 
            words: [
              'live', 'choir', 'chorus', 'medley', 'monologue',
-             'university', 'college', 'encore', 'duet',
+             'university', 'college', 'encore', 'duet', 'vs.', 'vs',
              'moshcam', 'premiere', 'dvd', 'Eurovision', 'sxsw',
              'improv', 'band', 'quartet', 'choral', 'chorale',
              'concerto', 'requiem', 'improvisation', 'recital', 'conducted',
@@ -18,14 +18,14 @@ var filter = function (songs, options) {
              'awards', 'music theory', 'arrangement', 'compilation',
              'lesson', 'tabs', 'tutorial', 'theme', 'kickstarter', 'session', 'sessions',
              'blog', 'vlog', 'concert', 'concierto', 'interview', 'soundtrack',
-             'chord changes', 'instrumental', 'episode',
+             'chord changes', 'instrumental', 'episode', 'test',
              'letterman', 'jimmy kimmel', 'jonathan ross', 'singing', 'ministry', 'community church',
              'me covering', 'guitar tab', 'anthem', 'plays', 'playing',
              'version', 'review', 'at the', 'OST', 'ukelele', 'lute', 'autoharp', 'guitar',
-             'poem', 'clarinet', 'dj', 'beat', 'download', 'ibiza', 'bass',
+             'poem', 'clarinet', 'dj', 'beat', 'download', 'd/l', 'ibiza', 'bass', 'preview', 'dub',
 
              'Justin Bieber', 'queen', 'One Direction', 'Michael Jackson', 'Temper Trap',
-             'dean martin', 'justin timberlake', 'cee lo green', 'josh groban',
+             'dean martin', 'justin timberlake', 'cee lo green', 'josh groban', 'harry potter',
              'britney spears', 'modest mouse', 'elton john', 'bob dylan', 'don mclean',
              'katy perry', 'johnny cash', 'snow patrol', 'nickleback', 'idina menzel',
              'timbaland', 'ingrid michaelson', 'shakira', 'lana del rey', 'alabama shakes',
@@ -34,11 +34,11 @@ var filter = function (songs, options) {
              'iron maiden', 'Guns N\' Roses', 'guns n roses', 'tom waits', 'REM', 'R.E.M',
              'R. Kelly', 'R.Kelly', 'bon iver', 'sinead o connor', 'norah jones', 'hq',
              'red hot chilli peppers', 'paul mcartney', 'taylor swift', 'jason derulo',
-             'billy bragg', 'miley cyrus', 'p!nk', 'modest mouse', 'wu tang clan',
+             'billy bragg', 'miley cyrus', 'p!nk', 'modest mouse', 'wu tang clan', 'jay z',
              'ACDC', 'AC/DC', 'freddie mercury', 'beach boys', 'kaiser chiefs', 'black eyed peas',
              'marvin gaye', 'chief keef', 'alicia keys', 'selena gomez', 'armand', 'paramore',
              'macklemore', 'queens of the stone age', 'the kinks', 'jimi hendrix', 'pitbull',
-             'twisted sister', '2pac', 'bjork', 'enya', 'jethro tull', 'mariah carey',
+             'twisted sister', '2pac', 'bjork', 'enya', 'jethro tull', 'mariah carey', 'otis redding',
              'lupe fiasco', 'my chemical romance', 'the beatles', 'maroon 5', 'carly rae jepsen',
              'the wanted', 'david bowie', 'rod stewart', 'rolling stones', 'skrillex', 'cher lloyd',
              'frank sinatra', 'kanye west', 'glee', 'linkin park', 'ella fitzgerald', 'billie holiday',
@@ -47,7 +47,7 @@ var filter = function (songs, options) {
            ],
            snippets: [
              'perform', 'interview',
-             '!!', '..', '+', '*', '??', '@','#','TEDx','.wmv', '.mov', '.avi', '.mp3', '.mpg'
+             '!!', '..', '+', '*', '??', '@','#','TEDx','.wmv', '.mov', '.avi', '.mp3', '.mpg', '.com'
            ]};
 
    if (options.exclude.covers) {
@@ -73,16 +73,10 @@ var filter = function (songs, options) {
 
       // Check if video has too few or too many views 
       if (song.listens > options.maxListens) {
-          console.log('skipping ' + song.pretty.title + ' for having too many listens');
-          console.log(song.listens);
-
          continue
       };
 
       if (song.listens < options.minListens) {
-         console.log('skipping ' + song.pretty.title + ' for having too few listens');
-         console.log(song.listens);
-
         continue
 
       };
@@ -94,7 +88,6 @@ var filter = function (songs, options) {
 
       // skip banned words
       if (hasBanned(song.pretty.title + ' ' + song.description, banned)) {
-         console.log('skipping ' + song.pretty.title);
          continue
       }
 
@@ -115,9 +108,6 @@ var filter = function (songs, options) {
       // Ignore too short or too long videos
       if (song.duration < options.minDuration ||
           song.duration > options.maxDuration)  {
-
-          console.log(song.source.name + ': ' + song.duration);
-
          continue
       };
 
@@ -127,8 +117,6 @@ var filter = function (songs, options) {
       for (var j = 0; j < results.length; j++) {
          var intersection = helper.intersect(song.pretty.title, results[j].pretty.title);
          if (intersection && intersection.length > 6) {
-            console.log('Ignoring: ' + song.pretty.title);
-            console.log(intersection);
             alreadyFromArtist = true;
          };
       };
@@ -153,14 +141,12 @@ var filter = function (songs, options) {
            if (inWord > -1) {
                var isWord = words.charAt(inWord - 1).trim() + words.charAt(inWord + list.words[i].length).trim();
                if (isWord.length === 0) {
-                  console.log('Has banned word ' + list.words[i]);
                   return true
                };
            };
        };
        for (var i in list.snippets) {
            if (words.indexOf(list.snippets[i]) > -1) {
-            console.log('Has banned word ' + list.snippets[i]);
                return true
            };
        };    
