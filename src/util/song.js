4ba457d5ae3cc72be339d2ed0e498e276809837f
David Merfield <dmerfield@gmail.com>
2014-07-03 23:51:59 -0700

    refactored song

--- src/util/song.js
+++ src/util/song.js
@@ -1,19 +1,36 @@
 Tuuune.Song = (function(){
 
   var template = 
-     '<span class="song" id="{{id}}">' +
-       '<span class="thumbnail" style="background: url({{thumbnail}}) no-repeat center center;background-size: cover"><img src="" /></span>' +
-       '<span class="hoverOverlay">' +
-         '<button class="play">&#9654; Play</button>' +
-         '<button class="addToQueue">+ Play next</button>' +
-       '</span>' +
-        '<button class="play">{{tinyTitle}}</button> ' +
-        '<button class="star" data-isStarred="{{isStarred}}">&#9733;</button>' +
-        '<span class="stats">' +
-          '<span class="duration">{{pretty.duration}} / </span>' +
-          '<span class="views">{{pretty.listens}} listens</span>' +
+       '<span class="song" data-id="{{id}}">' +
+         '<span class="thumbnail" style="background: url({{thumbnail}}) no-repeat center center;background-size: cover"><img src="" /></span>' +
+         '<span class="hoverOverlay">' +
+           '<button class="play" data-action="play">&#9654; Play</button>' +
+           '<button class="addToQueue" data-action="addToQueue">+ Play next</button>' +
+         '</span>' +
+          '<button class="title" data-action="play">{{tinyTitle}}</button> ' +
+          '<button class="star" data-action="star" data-isStarred="{{isStarred}}">&#9733;</button>' +
+          '<span class="stats">' +
+            '<span class="duration">{{pretty.duration}} / </span>' +
+            '<span class="views">{{pretty.listens}} listens</span>' +
+          '</span>' +
+       '</span>',
+
+     playerTemplate = 
+        '<span class="thumbnail" data-action="togglePlay" style="background: url({{thumbnail}}) no-repeat center center;background-size: cover"><img src="" /></span>' +
+        '<span class="title">{{pretty.title}}</span> ' +
+        '<span class="progressBar" data-action="setProgress">' +
+          '<span class="currentTime">0:00</span>' +
+          '<span class="progress"></span>' +
+          '<span class="duration">{{pretty.duration}}</span>' +
         '</span>' +
-     '</span>';
+        '<section class="controls">' +
+          '<button class="previous" data-action="previous"></button>' +
+          '<button class="togglePlay" data-action="togglePlay"></button>' +
+          '<button class="next" data-action="next"></button>' +
+          '<a class="permalink" data-action="permalink" target="_blank" href="{{source.permalink}}">&#9099;</a>' +
+          '<button class="star" data-action="star" data-isStarred="{{isStarred}}">&#9733;</button>' +
+        '</section>';
+
 
   function create (data) {
 
@@ -53,44 +70,60 @@ Tuuune.Song = (function(){
      return song
   };
 
-  function eventHandler (el, songs) {
+  function addListener (el, songs) {
 
-    // Import any modules we might need
-    var starred = include('starred'),
-        player = include('player'),
+    $(el).on('click', '[data-action]', function(e){
 
-        // Find the id of the song which was clicked
-        id = $(el).parents('.song').attr('id'),
+      // Import any modules we might need
+      var starred = include('starred'),
+          player = include('player'),
+          queue = include('queue'),
 
-        // Find the song from the id
-        song = songs ? songs.find(id) :
-                       player.queue().user.find(id),
+          // Find the id of the song which was clicked
+          id = $(this).parents('.song').attr('data-id'),
+
+          // Find the song from the id
+          song = songs ? songs.find(id) :
+                         queue.find(id),
+          
+          // Find what we need to do to the song
+          action = $(this).attr('data-action');
+
+      switch (action) {
         
-        // Find what we need to do to the song
-        action = $(el).attr('class');
+        case 'play':
+          return player.play(song, songs);          
 
-    switch (action) {
-      
-      case 'play':
-        return player.play(song, songs);          
-      
-      case 'addToQueue':
-        return player.addToQueue(song);
+        case "togglePlay":
+          return player.toggle();
 
-      case 'star':
-        return starred.toggle(song);
-      
-      case 'remove':
-        $('#' + id).remove();
-        return songs.remove(id);
-    };
+        case "next":
+          return player.play(queue.after(song));
 
+        case "previous":
+          return player.play(queue.before(song));
+
+        case "setProgress":
+          return player.setProgress(e.pageX);
+        
+        case 'addToQueue':
+          return queue.add(song);
+
+        case 'star':
+          return starred.toggle(song);
+        
+        case "permalink":
+          return player.pause();
+      };
+
+    });
   };
 
   return {
-    eventHandler: eventHandler,
+    addListener: addListener,
     create: create,
-    template: template
+    template: template,
+    playerTemplate: playerTemplate
   };
 
 }());
