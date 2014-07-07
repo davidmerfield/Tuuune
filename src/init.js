24a99a2279e5bdf6f33198d6808baffc6380bbeb
David Merfield <dmerfield@gmail.com>
2014-07-07 13:28:25 -0700

    added init to each module and show

--- src/init.js
+++ src/init.js
@@ -5,21 +5,25 @@ var Tuuune = {
 
 function include (name) {
   return Tuuune[name]
-};
+}
 
 $(function() {
 
-   var appPrefix = 'musicfinder',
-       nav = include('nav'),
-       player = include('player');
+  var appPrefix = 'musicfinder',
+      nav = include('nav'),
+      player = include('player'),
+      starred = include('starred');
+      songHistory = include('songHistory'),
+      discover = include('discover'),
+      queue = include('queue');
+
+  starred.init();
+  songHistory.init();
+  player.init();
+  queue.init();
+  discover.init();
 
   // Show the first view
   nav.init('discover');
-
-  // Load any songs which the user queued previously
-  // queue.user.add(storage.get(storageKey));
-
-  // Start the player
-  player.init();
-         
+       
 });
